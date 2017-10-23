29353ba96ca3bc3a3aac26af2a3afa0c2f78c498
sunnylia <1326155622@qq.com>
2017-10-23 19:20:33 +0800

    third commit

--- src/mobile/login/login.vue
+++ src/mobile/login/login.vue
@@ -5,12 +5,12 @@
       <form action="#" method="post">
         <p class="form_item"><input type="text" name="username" placeholder="邮箱/手机号/用户名"></p>
         <p class="form_item"><input type="password" name="pasword" placeholder="密码"></p>
-        <p class="form_btn"><button type="submit">登录</button></p>
+        <p class="form_btn"><button type="submit">登 录</button></p>
       </form>
     </div>
     <div class="login_other">使用其它方式登录 & 找回密码</div>
     <div class="regist_download">
-      <a href="#">注册豆瓣</a><a href="#">下载豆瓣App</a>
+      <a href="#">注册豆瓣</a><a href="#">下载豆瓣APP</a>
     </div>
   </div>
 </template>
@@ -25,12 +25,12 @@
 </script>
 <style scoped>
   .db_login {
-    font-size: 16px;
+    font-size: 0.6rem;
   }
   .db_login .db_login_top{
     position: relative;
-    height: 50px;
-    line-height: 50px;
+    height: 2rem;
+    line-height: 2rem;
     text-align: center;
     color: #333;
     font-weight: 600;
@@ -41,19 +41,21 @@
     top: 0;
     left: 0;
     display: block;
-    padding: 0 20px;
+    padding: 0 0.6rem;
     color: #42bd56;
-    font-size: 14px;
+    font-size: 0.6rem;
     font-weight: normal;
   }
+  input{
+    font-size: 0.6rem;
+  }
   .db_login .db_login_form{
-    padding: 0 15px;
-    margin: 25px 0 5px;
+    padding: 0.6rem 0.5rem;
   }
   .db_login .db_login_form .form_item{
-    height: 45px;
-    line-height: 45px;
-    padding: 0 10px;
+    height: 1.8rem;
+    line-height: 1.8rem;
+    padding: 0 0.4rem;
     margin-bottom: -1px;
     box-sizing: border-box;
     border: 1px solid #e0e0e0;
@@ -70,7 +72,7 @@
     border: none;
   }
   .db_login .db_login_form .form_btn{
-    margin-top: 10px;
+    margin-top: 1rem;
   }
   .db_login .form_btn button{
     -webkit-appearance: none;
@@ -78,26 +80,25 @@
     appearance: none;
     border: 0;
     width: 100%;
-    height: 40px;
-    line-height: 40px;
+    height: 1.8rem;
+    line-height: 1.8rem;
     text-align: center;
     color: #fff;
-    font-size: 14px;
+    font-size: 0.7rem;
     background-color: #17AA52;
-    border-radius: 3px;
+    border-radius: 0.2rem;
   }
   .db_login .login_other{
     color: #9d9d9d;
     text-align: center;
-    margin-top: 15px;
+    margin: 0.5rem 0 0.6rem 0;
   }
   .db_login .regist_download{
     text-align: center;
-    margin-top: 40px;
   }
   .db_login .regist_download a{
     color: #42bd56;
-    font-size: 14px;
-    margin: 0 10px;
+    font-size: 0.6rem;
+    margin: 0 0.5rem;
   }
 </style>
